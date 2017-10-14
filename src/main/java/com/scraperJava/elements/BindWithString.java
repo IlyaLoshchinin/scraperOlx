0a0d1e825219e89198cbce455ee679592ad2376d
ILLIA <illia.loshchinin@gmail.com>
2017-10-14 21:39:57 +0200

    implement to  bind field with part of url

---
+++ src/main/java/com/scraperJava/elements/BindWithString.java
@@ -0,0 +1,14 @@
+package com.scraperJava.elements;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by Ilya Loshchinin on 14.10.2017.
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BindWithString {
+       String value();
+}
